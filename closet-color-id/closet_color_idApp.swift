81de54ea2b2e4e3ac326fea45840d30b64766a1b
Wave Chin <wbchin@andrew.cmu.edu>
2022-10-08 21:22:56 -0400

    init

---
+++ closet-color-id/closet_color_idApp.swift
@@ -0,0 +1,17 @@
+//
+//  closet_color_idApp.swift
+//  closet-color-id
+//
+//  Created by Waverly Chin on 10/8/22.
+//
+
+import SwiftUI
+
+@main
+struct closet_color_idApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
