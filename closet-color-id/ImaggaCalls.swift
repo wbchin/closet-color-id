d67c82dc8a4ed50e4fd595becd59a0a24c95cf37
wbchin <wbchin@andrew.cmu.edu>
2022-11-15 14:08:52 -0500

    Color api implemented into save article

--- closet-color-id/ImaggaCalls.swift
+++ closet-color-id/ImaggaCalls.swift
@@ -71,18 +71,6 @@ class ImaggaCalls: ObservableObject {
                                           else {
                                               return nil
                                           }
-                                          print("IMAGGA NAME AND FAM")
-                                          print(name)
-                                          print(family)
-                                          self.colorApiCall.fetchAlamo(rgb: "\(r),\(g),\(b)", completion: { n in
-                                              if n {
-                                                  name = self.colorApiCall.seedName!
-                                                  family = self.viewModel.setColorFamily(hue: self.colorApiCall.seedHue!)
-                                              }
-                                          }) //UNSURE IF THIS IS RESETTING FAMILY AND NAME
-                                          print("COLOR API NAME AND FAM")
-                                          print(name)
-                                          print(family)
                                           return PhotoColor(
                                               primaryHex: hex,
                                               primaryName: name,
@@ -93,19 +81,8 @@ class ImaggaCalls: ObservableObject {
                                       })
                                       self.colors = photoColors
                                     if photoColors.count == 1 {
-                                        print("PHOTO COLOR NAME AND FAM 1")
-                                        print(photoColors.first?.primaryName)
-                                        print(photoColors.first?.primaryFamily)
-                                        print(self.colorApiCall.seedName!)
-                                        print(self.viewModel.setColorFamily(hue: self.colorApiCall.seedHue!))
-                                        print(photoColors.first?.primaryFamily)
                                       self.article = self.viewModel.saveArticle(image_data: self.image!.pngData()!, primary_color_name: photoColors.first!.primaryName, primary_color_family: photoColors.first!.primaryFamily, primary_r: photoColors.first!.r, primary_g: photoColors.first!.g, primary_b: photoColors.first!.b, secondary_color_name: nil, secondary_color_family: nil, secondary_r: nil, secondary_g: nil, secondary_b: nil)!
                                     } else {
-                                        print("PHOTO COLOR NAME AND FAM 2")
-                                        print(photoColors.first?.primaryName)
-                                        print(photoColors.first?.primaryFamily)
-                                        print(photoColors[1].primaryName)
-                                        print(photoColors[1].primaryFamily)
                                       self.article = self.viewModel.saveArticle(image_data: self.image!.pngData()!, primary_color_name: photoColors.first!.primaryName, primary_color_family: photoColors.first!.primaryFamily, primary_r: photoColors.first!.r, primary_g: photoColors.first!.g, primary_b: photoColors.first!.b, secondary_color_name: photoColors[1].primaryName, secondary_color_family: photoColors[1].primaryFamily, secondary_r: photoColors[1].r, secondary_g: photoColors[1].g, secondary_b: photoColors[1].b)!
                                     }
                                   } catch{
